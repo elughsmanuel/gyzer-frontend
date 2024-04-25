94b4a11994877c8bdd5a6e69b29e8f7b5d080c74
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2024-04-25 13:35:29 +0100

    feat: add evaluatios items

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,6 +9,7 @@ const Dashboard = () => {
     const [allEvaluations, setAllEvaluations] = useState("");
     const [sentEvaluations, setSentEvaluations] = useState("");
     const [receivedEvaluations, setReceivedEvaluations] = useState("");
+    const [allEvaluationItems, setAllEvaluationsItems] = useState([]);
 
     const navigate = useNavigate();
 
@@ -42,6 +43,9 @@ const Dashboard = () => {
 
                 const allEvaluationsData = allEvaluationResponse.data.data.length;
                 setAllEvaluations(allEvaluationsData);
+
+                const allEvaluationsItemsData = allEvaluationResponse.data.data;
+                setAllEvaluationsItems(allEvaluationsItemsData);
 
                 const sentEvaluationResponse = await axios.get("http://localhost:8000/api/v1/evaluations/get-my-evaluations?type=sent", {
                     headers: {
@@ -77,24 +81,40 @@ const Dashboard = () => {
 
     return (
         <div className="dashboard__box">
-        <Navbar firstName={firstName} handleSignOut={handleSignOut} />
-        <div className="main-content">
-            <h2 className="evaluation-text">Evaluations</h2>
-        <div className="eva-box">
-        <div className="evaluation-box">
-            <h3>All</h3>
-            <h4>{allEvaluations}</h4>
-        </div>
-        <div className="evaluation-box">
-            <h3>Sent</h3>
-            <h4>{sentEvaluations}</h4>
-        </div>
-        <div className="evaluation-box">
-            <h3>Received</h3>
-            <h4>{receivedEvaluations}</h4>
-        </div>
-        </div>
-      </div>
+            <div className="main-content">
+                <Navbar firstName={firstName} handleSignOut={handleSignOut} />
+                <h2 className="evaluation-text">Evaluations</h2>
+                <div className="eva-box">
+                    <div className="evaluation-box">
+                        <h3>All</h3>
+                        <h4>{allEvaluations}</h4>
+                    </div>
+                    <div className="evaluation-box">
+                        <h3>Sent</h3>
+                        <h4>{sentEvaluations}</h4>
+                    </div>
+                    <div className="evaluation-box">
+                        <h3>Received</h3>
+                        <h4>{receivedEvaluations}</h4>
+                    </div>
+                </div>
+            </div>
+            <div className="evaluation-items">
+                    <h2>All Evaluations</h2>
+                    {allEvaluationItems.map((evaluation) => (
+                        <div key={evaluation.id} className="evaluation-item">
+                            <h3>{evaluation.month} {evaluation.year}</h3>
+                            <p><strong>Work Quality:</strong> {evaluation.workQualityScore}</p>
+                            <p ><strong>Work Quality Comment:</strong> {evaluation.workQualityComment}</p>
+                            <p><strong>Task Completion:</strong> {evaluation.taskCompletionScore}</p>
+                            <p><strong>Task Completion Comment:</strong> {evaluation.taskCompletionComment}</p>
+                            <p><strong>Above and Beyond:</strong> {evaluation.aboveAndBeyondScore}</p>
+                            <p><strong>Above And Beyond Comment:</strong> {evaluation.aboveAndBeyondComment}</p>
+                            <p><strong>Communication:</strong> {evaluation.communicationScore}</p>
+                            <p><strong>Communication Comment:</strong> {evaluation.communicationComment}</p>
+                        </div>
+                    ))}
+                </div>
       </div>
     );
 };
